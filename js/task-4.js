515e800e8e46a83aa7068984ec278235a566c157
SerhiiStepanovich <bysprint82@gmail.com>
2025-06-21 13:18:56 +0300

    HW-07-task-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  const emailValue = email.emailValue.trim();
+  const passwordlValue = password.passwordValue.trim();
+
+  if (!emailValue || !passwordlValue) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordlValue,
+  };
+  console.log(formData);
+  event.currentTarget.reset();
+});
